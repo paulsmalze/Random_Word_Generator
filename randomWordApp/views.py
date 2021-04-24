b7f9e17d62e1c3f7195fd12c079f5bd1d698ec22
paulsmalze <waffwegs@yahoo.com>
2021-04-24 17:34:25 -0500

    added request.session

--- randomWordApp/views.py
+++ randomWordApp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
-#from django.utils.crypto import get_random_string
+from django.utils.crypto import get_random_string
 
 # Create your views here.
 
 def randomWord(request):
-   # request.session['word'] = get_random_string(length=14)
+    if 'count' not in request.session:
+        request.session['count'] = 0
+    request.session['word'] = get_random_string(length=14)
+    request.session['count'] += 1
     return render(request, 'index.html')
